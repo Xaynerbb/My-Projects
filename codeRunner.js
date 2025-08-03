333bbbb1135bb534e77ee0a3afbc1fa3293fa238
Xaynerbb <zainablazeez22@gmail.com>
2025-08-03 17:07:09 +0100

    Practice

---
+++ codeRunner.js
@@ -0,0 +1,20 @@
+// This is your starting point for writing JavaScript code
+console.log("Hello, World!");
+
+// You can add your JavaScript logic below
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("User"));
+console.log(2+5);
+console.log(typeof 4);
+let name = 'Zainab';
+console.log(`My name is ${name}`);
+let cost = `$(${(5 + (2*3) + 9) })`;
+console.log(`Cost of food: ${cost}`);
+cost = `${(5 + (2*3) + 9) }`;
+let tax = cost * 0.1;
+console.log(`Tax(10%): $${tax}`);
+let totalCost= `${cost + tax} `; //how to add up two variables
+console.log( `Total cost: $${totalCost}`);
---
+++ w3schools.js
@@ -0,0 +1,2 @@
+console.log('Zainab');
+console.log('Where is my run button');
